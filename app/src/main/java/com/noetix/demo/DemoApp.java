e1475bda5f1558f022dad3e956b809c647a63bec
noetix <lpjhblpj@163.com>
2025-07-26 18:26:55 +0800

    demo

--- app/src/main/java/com/noetix/demo/DemoApp.java
+++ app/src/main/java/com/noetix/demo/DemoApp.java
@@ -9,8 +9,8 @@ public class DemoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        IRobotSDKManager.getInstance().prepareSerialPortAndCanBusDevice("/dev/ttyS1", "/dev/ttyACM0");
-        IRobotSDKManager.getInstance().init();
+//        IRobotSDKManager.getInstance().init("/dev/ttyS1", "/dev/ttyACM0");
+        IRobotSDKManager.getInstance().init("", "");
         IRobotSDKManager.getInstance().enableLog(true);
         IRobotSDKManager.getInstance().saveAudioData(true);
     }
