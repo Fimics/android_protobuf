87cab091bd6bf12175bd724af9a4431b634c9385
noetix <lpjhblpj@163.com>
2025-10-15 11:03:58 +0800

    去掉签名

--- app/src/main/java/com/noetix/demo/DemoApp.java
+++ app/src/main/java/com/noetix/demo/DemoApp.java
@@ -9,7 +9,8 @@ public class DemoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        CANShell.executeCanCommands();
+//        CANShell.executeCanCommands();
+//        android:sharedUserId="android.uid.system"
         IRobotSDKManager.getInstance().enableLog(true);
         IRobotSDKManager.getInstance().init("","",2,false);
         IRobotSDKManager.getInstance().setNeckRadiosDuration(new float[]{0.0f, 0.0f, 0.0f}, 5.0f);
